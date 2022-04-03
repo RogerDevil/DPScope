be805790bc78564d32d9e2b70b7219615190a21c
KC Lee <roger.devil@gmail.com>
2022-04-03 16:32:31 +0100

    Reordered when high.Plotter() is called with respect to the view from the main script. Removed unnecessary view imports from the main file.

--- dpscope/gui.py
+++ dpscope/gui.py
@@ -1,24 +1,10 @@
-import matplotlib
-matplotlib.use('TkAgg')
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 from portselect import get_port
-from tkinter import (Tk, Frame, LabelFrame, BOTH, Button, Label, Spinbox, X,
-                     LEFT, Scale, Checkbutton, BooleanVar, W, StringVar,
-                     OptionMenu, Radiobutton, HORIZONTAL, IntVar, E)
 
 import high
 from model.controller.helper.trigger import TriggerSource
 from model.controller.helper.voltage_measure import VoltageResolution
 from view.director import Director
 from view.builder.standard import StandardViewBuilder
-
-root = Tk()
-root.title("DPScope")
-fig = Figure()
-
-
-pltr = high.Plotter(fig)
 
 class Datalogger(high.Task):
     def __init__(self, widget, interval):
@@ -37,7 +23,7 @@ stopfn = lambda: None
 def start():
     global stopfn
     if view.signals['Horizontal.sample_mode'] in "Datalog mode":
-        dl = Datalogger(root, 100)
+        dl = Datalogger(view.window, 100)
         stopfn = dl.stop
         dl.start()
 
@@ -49,6 +35,7 @@ view_builder = Director(StandardViewBuilder())
 view_builder.view_build()
 view = view_builder.view_get()
 
+pltr = high.Plotter(view.fig)
 pltr.scope = get_port(view.window)
 
 with pltr.scope as dpscope:
