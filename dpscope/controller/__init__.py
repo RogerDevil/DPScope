3dae5c2bcf4df2f4ba09947fbdb0158badd906bf
KC Lee <roger.devil@gmail.com>
2022-05-03 00:05:35 +0100

    Defined AcquisitionRate() helper class for mapping acquisition rate options in view to actual measurement rates in model.

--- dpscope/controller/__init__.py
+++ dpscope/controller/__init__.py
@@ -3,11 +3,3 @@ Coordinates model and view interactions.
 
 This is the controller from the MVC design pattern.
 """
-import logging
-
-# Set up logging
-logging.basicConfig(level=logging.DEBUG)
-_LOGGER = logging.getLogger(__name__)
-_LOGGER.setLevel(logging.DEBUG)
-
-
